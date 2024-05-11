0c083647cfc065a0003489d38f41aa8c8c9ecb92
tji98d <kaiquenropsg@hotmail.com>
2024-05-12 05:44:14 +0700

    update project

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hi");
+function numDistinct(s, t) {
+  const m = s.length;
+  const n = t.length;
+  const dp = Array.from(Array(m + 1), () => Array(n + 1).fill(0));
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = 1;
+  }
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (s[i - 1] === t[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+      } else {
+        dp[i][j] = dp[i - 1][j];
+      }
+    }
+  }
+  return dp[m][n];
+}
