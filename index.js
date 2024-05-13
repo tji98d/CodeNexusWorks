822125d72047cc95b98509ddd1791365fc775ac7
tji98d <kaiquenropsg@hotmail.com>
2024-05-14 01:09:45 +0700

    demo project

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const deepFlatten = (arr) =>
-  [].concat(...arr.map((v) => (Array.isArray(v) ? deepFlatten(v) : v)));
+function subsetsWithDup(nums) {
+  const result = [];
+  nums.sort((a, b) => a - b);
+  backtrack(0, []);
+  return result;
+  function backtrack(start, current) {
+    result.push([...current]);
+    for (let i = start; i < nums.length; i++) {
+      if (i > start && nums[i] === nums[i - 1]) continue;
+      current.push(nums[i]);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
+}
