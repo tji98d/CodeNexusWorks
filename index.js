1d3c365d98476c0e83f4b471746e005c6671f346
tji98d <kaiquenropsg@hotmail.com>
2024-05-16 12:34:46 +0700

    hi commit

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-function merge(intervals) {
-  intervals.sort((a, b) => a[0] - b[0]);
-  const merged = [];
-  for (const interval of intervals) {
-    if (!merged.length || merged[merged.length - 1][1] < interval[0]) {
-      merged.push(interval);
-    } else {
-      merged[merged.length - 1][1] = Math.max(
-        merged[merged.length - 1][1],
-        interval[1],
-      );
-    }
-  }
-  return merged;
-}
+const sum = (a, b) => a + b;
